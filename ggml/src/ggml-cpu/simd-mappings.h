f83470e931f237f16ede70ee68e4784f3ffb10af
Aaron Teo <aaron.teo1@ibm.com>
2025-04-09 21:53:11 +0800

    ggml: refactor to type-cast and update documentation
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>
    
    Co-authored-by: Aleksei Nikiforov <aleksei.nikiforov@ibm.com>

--- ggml/src/ggml-cpu/simd-mappings.h
+++ ggml/src/ggml-cpu/simd-mappings.h
@@ -851,17 +851,15 @@ static inline __vector float __lzs_f16cx4_load(const ggml_fp16_t * x) {
         tmp[i] = GGML_FP16_TO_FP32(x[i]);
     }
 
-    // TODO: change to const pointer instead (see #12846)
-    // this is only a workaround to the compile error but its unreadable
-    return vec_xl(0, &(tmp[0]));
+    // note: keep type-cast here to prevent compiler bugs (see #12846)
+    return vec_xl(0, (const float *)(tmp));
 }
 
 static inline void __lzs_f16cx4_store(ggml_fp16_t * x, __vector float y) {
     float arr[4];
 
-    // TODO: change to const pointer instead (see #12846)
-    // this is only a workaround to the compile error but its unreadable
-    vec_xst(y, 0, &(arr[0]));
+    // note: keep type-cast here to prevent compiler bugs (see #12846)
+    vec_xst(y, 0, (float *)(arr));
 
     for (int i = 0; i < 4; i++) {
         x[i] = GGML_FP32_TO_FP16(arr[i]);

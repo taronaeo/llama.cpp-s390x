8f7fb603fa3c772540578cfac23b9bba1b81abdb
Aaron Teo <aaron.teo1@ibm.com>
2025-06-07 22:39:13 +0800

    ggml-zdnn: disable tracing op matmul
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ggml/src/ggml-zdnn/ggml-zdnn.cpp
+++ ggml/src/ggml-zdnn/ggml-zdnn.cpp
@@ -254,8 +254,6 @@ static bool ggml_zdnn_compute_forward(ggml_backend_zdnn_context & ctx,
             ggml_zdnn_op_bin<zdnn_norm>(ctx, dst);
             break;
         case GGML_OP_MUL_MAT:
-            std::raise(SIGINT);
-            break;
         case GGML_OP_MUL_MAT_ID:
         case GGML_OP_SOFT_MAX:
             // ggml_zdnn_op_activation<zdnn_softmax>(ctx, dst);
@@ -509,8 +507,6 @@ static bool ggml_backend_zdnn_device_supports_op(ggml_backend_dev_t dev, const s
         case GGML_OP_NORM:
             return true;
         case GGML_OP_MUL_MAT:
-            // TODO: Disable this one debugging is complete
-            return true;
         case GGML_OP_MUL_MAT_ID:
         case GGML_OP_SOFT_MAX:
         case GGML_OP_LEAKY_RELU:

b62c93efc811e4302ac16535c7332f6e79c71db8
Aaron Teo <aaron.teo1@ibm.com>
2025-11-01 18:32:02 +0800

    ggml: fix review comments
    * drop vxe feature
    * add nnpa feature
    
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ggml/src/ggml-cpu/arch/s390/cpu-feats.cpp
+++ ggml/src/ggml-cpu/arch/s390/cpu-feats.cpp
@@ -4,10 +4,6 @@
 #include <sys/auxv.h>
 
 // find hwcap bits in asm/elf.h
-#ifndef HWCAP_VXRS_EXT
-#define HWCAP_VXRS_EXT (1 << 13)
-#endif
-
 #ifndef HWCAP_VXRS_EXT2
 #define HWCAP_VXRS_EXT2 (1 << 15)
 #endif
@@ -17,7 +13,6 @@
 #endif
 
 struct s390x_features {
-    bool has_vxe  = false;
     bool has_vxe2 = false;
     bool has_nnpa = false;
 
@@ -26,7 +21,6 @@ struct s390x_features {
         // NOTE: use hwcap2 with DFLT for z17 and later
         // uint32_t hwcap2 = getauxval(AT_HWCAP2);
 
-        has_vxe  = !!(hwcap & HWCAP_VXRS_EXT);
         has_vxe2 = !!(hwcap & HWCAP_VXRS_EXT2);
         has_nnpa = !!(hwcap & HWCAP_NNPA);
     }
@@ -36,17 +30,16 @@ static int ggml_backend_cpu_s390x_score() {
     int score = 1;
     s390x_features sf;
 
-#ifdef GGML_USE_VXE
-    if (!sf.has_vxe) { return 0; }
-    score += 1 << 1;
-#endif
+// IBM z15 / LinuxONE 3
 #ifdef GGML_USE_VXE2
     if (!sf.has_vxe2) { return 0; }
-    score += 1 << 2;
+    score += 1 << 1;
 #endif
+
+// IBM z16 / LinuxONE 4 and z17 / LinuxONE 5
 #ifdef GGML_USE_NNPA
     if (!sf.has_nnpa) { return 0; }
-    score += 1 << 3;
+    score += 1 << 2;
 #endif
 
     return score;

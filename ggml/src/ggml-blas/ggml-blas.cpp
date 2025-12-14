aae6d1e9b0adfd73db507fc3e29bd5acf542e9d7
Aaron Teo <aaron.teo1@ibm.com>
2025-12-14 19:15:34 +0800

    ggml-blas: fix invalid data access
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- ggml/src/ggml-blas/ggml-blas.cpp
+++ ggml/src/ggml-blas/ggml-blas.cpp
@@ -515,6 +515,8 @@ static bool ggml_backend_blas_device_supports_op(ggml_backend_dev_t dev, const g
 
             return ggml_is_contiguous(src0) &&
                    ggml_is_contiguous(src1) &&
+                   src0->view_src == nullptr &&
+                   src1->view_src == nullptr &&
                    src1->type == GGML_TYPE_F32 &&
                    (ne0 >= min_batch && ne1 >= min_batch && ne10 >= min_batch) &&
                    (src0->type == GGML_TYPE_F32 || ggml_get_type_traits(src0->type)->to_float != NULL);

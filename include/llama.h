9b1a39e0a7c6dab228855514807857d76d97735f
Aaron Teo <aaron.teo1@ibm.com>
2026-04-14 23:09:19 +0800

    llama: completely remove old flags
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- include/llama.h
+++ include/llama.h
@@ -322,9 +322,6 @@ extern "C" {
 
         // Keep the booleans together to avoid misalignment during copy-by-value.
         bool vocab_only;      // only load the vocabulary, no weights
-        // bool use_mmap;        // DEPRECATED: use mmap if possible
-        // bool use_direct_io;   // DEPRECATED: use direct io, takes precedence over use_mmap when supported
-        // bool use_mlock;       // DEPRECATED: force system to keep model in RAM
         bool check_tensors;   // validate model tensor data
         bool use_extra_bufts; // use extra buffer types (used for weight repacking)
         bool no_host;         // bypass host buffer allowing extra buffers to be used
